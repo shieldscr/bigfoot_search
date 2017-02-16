00137768fcf6d45f03b87b9063671197418f525b
Cory Shields <shieldscr88@gmail.com>
2017-02-16 16:17:55 -0500

    Searching by type now based on user input

--- bigfoot_app/search.js
+++ bigfoot_app/search.js
@@ -2,18 +2,22 @@ $("document").ready(function() {
   $("#search-form").submit(function(event){
     event.preventDefault();
     var searchInput = $('#search-form :input').val();
+    var type = searchInput.split(' ')[0];
+    var searchTerm = searchInput.split(' ')[1];
 
     $.ajax({
-      url: 'http://localhost:9200/bigfoot/footprint/_search?q=' + searchInput,
+      url: 'http://localhost:9200/bigfoot/' + type + '/_search?q=' + searchInput,
       type: 'GET',
       dataType: 'json',
       success: function (data, textStatus, xhr) {
+
+        console.log('search: ' + 'http://localhost:9200/bigfoot/' + type + '/_search?q=' + searchInput);
 
         var jsonList = [];
 
         $.each(data.hits.hits[0]._source, function(key, value) {
           if(value) {
-            if(String(value).includes(searchInput)) {
+            if(String(value).includes(searchTerm)) {
               jsonList.push('<tr><td>' + key.replace('f_', '') + '</td><td><mark>' + value + '</mark></td></tr>');
             } else {
               jsonList.push('<tr><td>' + key.replace('f_', '') + '</td><td>' + value + '</td></tr>');
