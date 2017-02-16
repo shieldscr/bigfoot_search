81e165e2c6e9389784d968b7b806aca03ec83aa0
Cory Shields <shieldscr88@gmail.com>
2017-02-15 20:35:05 -0500

    Search is formatting output better

--- bigfoot_app/search.js
+++ bigfoot_app/search.js
@@ -1,26 +1,28 @@
 $("document").ready(function() {
   $("#search-form").submit(function(event){
     event.preventDefault();
-    var searchInput = $(this).serialize();
+    var searchInput = $('#search-form :input').val();
+
     $.ajax({
-            url: 'http://localhost:9200/bigfoot/footprint/_search?q=' + searchInput,
-            type: 'GET',
-            dataType: 'json',
-            success: function (data, textStatus, xhr) {
-              var jsonList = [];
+      url: 'http://localhost:9200/bigfoot/footprint/_search?q=' + searchInput,
+      type: 'GET',
+      dataType: 'json',
+      success: function (data, textStatus, xhr) {
 
-              $.each(data.hits.hits[0]._source, function(key, value) {
-                if(value) {
-                  jsonList.push('<tr><td>' + key.replace('f_', '') + '</td><td>' + value + '</td></tr>');
-                }
-              });
+        var jsonList = [];
 
-              $("#result").append('<table>' + jsonList + '</table>');
+        $.each(data.hits.hits[0]._source, function(key, value) {
+          if(value) {
+            jsonList.push('<tr><td>' + key.replace('f_', '') + '</td><td>' + value + '</td></tr>');
+          }
+        });
 
-            },
-            error: function (xhr, textStatus, errorThrown) {
-              console.log(errorThrown);
-            }
-          });
+        $("#result").html('<table>' + jsonList.join("") + '</table>');
+
+      },
+      error: function (xhr, textStatus, errorThrown) {
+        console.log(errorThrown);
+      }
+    });
   });
 });
