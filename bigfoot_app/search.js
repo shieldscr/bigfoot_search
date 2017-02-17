53dd23406980732ab352932d3e774d2e216131df
Cory Shields <shieldscr88@gmail.com>
2017-02-16 20:46:42 -0500

    Displaying all results

--- bigfoot_app/search.js
+++ bigfoot_app/search.js
@@ -14,23 +14,29 @@ $("document").ready(function() {
         console.log('search: ' + 'http://localhost:9200/bigfoot/' + type + '/_search?q=' + searchTerm);
 
         var outputList = [];
+        var finalHtml = [];
 
         if(data.hits.hits[0]) {
-          $.each(data.hits.hits[0]._source, function(key, value) {
-            if(value) {
-              if(String(value).includes(searchTerm)) {
-                outputList.push('<tr><td>' + key.replace('f_', '') + '</td><td><mark>' + value + '</mark></td></tr>');
-              } else {
-                outputList.push('<tr><td>' + key.replace('f_', '') + '</td><td>' + value + '</td></tr>');
+          $.each(data.hits.hits, function(key, value) {
+            $.each(value._source, function(key, value) {
+              if(value) {
+                if(String(value).includes(searchTerm)) {
+                  outputList.push('<tr><td>' + key.replace('f_', '') + '</td><td><mark>' + value + '</mark></td></tr>');
+                } else {
+                  outputList.push('<tr><td>' + key.replace('f_', '') + '</td><td>' + value + '</td></tr>');
+                }
               }
+            });
+            outputList.push('<tr class="end-table-row"><td></td><td></td></tr>');
+            outputList.push('<tr class="end-table-row"><td></td><td></td></tr>');
+
+            if(outputList.length > 0) {
+              finalHtml.push('<table class="striped">' + outputList.join("") + '</table>');
+            } else {
+              finalHtml.push('<h3 class="medium center pad-top">No results found. Bigfoot walks the Earth...</h3>');
             }
           });
-          outputList.push('<tr class="end-table-row"><td></td><td></td></tr>');
-          outputList.push('<tr class="end-table-row"><td></td><td></td></tr>');
-
-          $("#result").html('<table class="striped">' + outputList.join("") + '</table>');
-        } else {
-          $("#result").html('<h3 class="medium center pad-top">No results found. Bigfoot walks the Earth...</h3>');
+          $("#result").html(finalHtml);
         }
       },
       error: function (xhr, textStatus, errorThrown) {
