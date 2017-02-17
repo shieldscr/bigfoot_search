71b27495b61cb8624b74841bfe798f8966c91f8f
Cory Shields <shieldscr88@gmail.com>
2017-02-16 22:11:42 -0500

    Fixed duplicate search results

--- bigfoot_app/search.js
+++ bigfoot_app/search.js
@@ -15,6 +15,7 @@ $("document").ready(function() {
 
         var outputList = [];
         var finalHtml = [];
+        var resultCount = data.hits.hits.length;
 
         if(data.hits.hits[0]) {
           $.each(data.hits.hits, function(key, value) {
@@ -27,16 +28,18 @@ $("document").ready(function() {
                 }
               }
             });
-            outputList.push('<tr class="end-table-row"><td></td><td></td></tr>');
-            outputList.push('<tr class="end-table-row"><td></td><td></td></tr>');
+
+            buildTablePadding(outputList);
 
             if(outputList.length > 0) {
               finalHtml.push('<table class="striped">' + outputList.join("") + '</table>');
             } else {
               finalHtml.push('<h3 class="medium center pad-top">No results found. Bigfoot walks the Earth...</h3>');
             }
+            outputList = [];
           });
-          $("#result").html(finalHtml);
+          var resultsStats = '<p>Your search returned ' + resultCount + ' results:</p><br />';
+          $("#result").html(resultsStats + finalHtml.join(""));
         }
       },
       error: function (xhr, textStatus, errorThrown) {
@@ -44,4 +47,9 @@ $("document").ready(function() {
       }
     });
   });
+
+  function buildTablePadding(outputList) {
+    outputList.push('<tr class="end-table-row"><td></td><td></td></tr>');
+    outputList.push('<tr class="end-table-row"><td></td><td></td></tr>');
+  }
 });
